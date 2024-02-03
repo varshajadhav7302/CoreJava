e56a19da425aff811c22b996e8f4e0c295a47fff
varshajadhav7302 <118867027+varshajadhav7302@users.noreply.github.com>
2024-02-03 19:47:41 +0530

    Assignment

---
+++ src/AssignmentArray5/FrequencyArrayCount.java
@@ -0,0 +1,19 @@
+package AssignmentArray5;
+
+public class FrequencyArrayCount {
+
+	
+	public static void main(String[] args) {
+		int arr [] = {2, 2, 3, 4, 3, 4, 2};
+		
+		for(int i = 0; i > arr.length; i++) {
+			
+			//int val = 0;
+			
+			//int val = i[0];
+			
+			
+			
+		}
+	}
+}
